240b3b250f9bd59f9850fdec413b43c9ff9aa5dc
jos3barros <barros_j@yahoo.com>
2018-02-26 15:40:36 -0300

    Initial Commit

---
+++ Receipts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Receipts
+//
+//  Created by Jose on 26/2/18.
+//  Copyright © 2018 appcat.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Receipts/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Receipts
+//
+//  Created by Jose on 26/2/18.
+//  Copyright © 2018 appcat.com. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
